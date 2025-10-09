17ba77750436c241c4c2640ae0fde8d3ccf37e62
hhhxwhhh <2093983190@qq.com>
2025-10-09 18:45:20 +0800

    删除fe部署时有关git的要求

--- OnlineJudgeFE/config/dev.env.js
+++ OnlineJudgeFE/config/dev.env.js
@@ -1,11 +1,24 @@
-let date = require('moment')().format('YYYYMMDD')
-let commit = require('child_process').execSync('git rev-parse HEAD').toString().slice(0, 5)
-let version = `"${date}-${commit}"`
+let date = require("moment")().format("YYYYMMDD");
+let commit;
 
-console.log(`current version is ${version}`)
+// 尝试执行 git 命令，失败时使用默认值
+try {
+  commit = require("child_process")
+    .execSync("git rev-parse HEAD", {
+      stdio: ["pipe", "pipe", "ignore"],
+    })
+    .toString()
+    .slice(0, 5);
+} catch (e) {
+  commit = "dev";
+}
+
+let version = `'${date}-${commit}'`;
+
+console.log(`current version is ${version}`);
 
 module.exports = {
-  NODE_ENV: '"development"',
+  NODE_ENV: "development",
   VERSION: version,
-  USE_SENTRY: '0'
-}
+  USE_SENTRY: "0",
+};
