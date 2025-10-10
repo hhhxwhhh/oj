b4939aed73a8dc6cfc27a2d99aa38c169cf39476
hhhxwhhh <2093983190@qq.com>
2025-10-10 18:26:27 +0800

    ``` build(webpack): 修复重复模块和别名配置问题
    在 webpack.base.conf.js 文件中，发现 modules 和 alias 配置项存在大量重复条目，
    可能导致构建异常或性能问题。此次修改清理了重复的 simple-module、simple-hotkeys
    和 simple-uploader 别名以及 modules 路径配置，确保每项仅出现一次，提升配置清晰度。
    
    同时更新了 nginx.conf 中的后端开发服务器地址，将 upstream oj-backend-dev
    指向新的 IP 地址 172.18.0.5，以匹配当前部署环境。
    ```

---
+++ build/CMakeFiles/libjudger.so.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/wang/oj/Judger/src/argtable3.c" "CMakeFiles/libjudger.so.dir/src/argtable3.c.o" "gcc" "CMakeFiles/libjudger.so.dir/src/argtable3.c.o.d"
+  "/Users/wang/oj/Judger/src/child.c" "CMakeFiles/libjudger.so.dir/src/child.c.o" "gcc" "CMakeFiles/libjudger.so.dir/src/child.c.o.d"
+  "/Users/wang/oj/Judger/src/killer.c" "CMakeFiles/libjudger.so.dir/src/killer.c.o" "gcc" "CMakeFiles/libjudger.so.dir/src/killer.c.o.d"
+  "/Users/wang/oj/Judger/src/logger.c" "CMakeFiles/libjudger.so.dir/src/logger.c.o" "gcc" "CMakeFiles/libjudger.so.dir/src/logger.c.o.d"
+  "/Users/wang/oj/Judger/src/main.c" "CMakeFiles/libjudger.so.dir/src/main.c.o" "gcc" "CMakeFiles/libjudger.so.dir/src/main.c.o.d"
+  "/Users/wang/oj/Judger/src/rules/c_cpp.c" "CMakeFiles/libjudger.so.dir/src/rules/c_cpp.c.o" "gcc" "CMakeFiles/libjudger.so.dir/src/rules/c_cpp.c.o.d"
+  "/Users/wang/oj/Judger/src/rules/c_cpp_file_io.c" "CMakeFiles/libjudger.so.dir/src/rules/c_cpp_file_io.c.o" "gcc" "CMakeFiles/libjudger.so.dir/src/rules/c_cpp_file_io.c.o.d"
+  "/Users/wang/oj/Judger/src/rules/general.c" "CMakeFiles/libjudger.so.dir/src/rules/general.c.o" "gcc" "CMakeFiles/libjudger.so.dir/src/rules/general.c.o.d"
+  "/Users/wang/oj/Judger/src/rules/golang.c" "CMakeFiles/libjudger.so.dir/src/rules/golang.c.o" "gcc" "CMakeFiles/libjudger.so.dir/src/rules/golang.c.o.d"
+  "/Users/wang/oj/Judger/src/rules/node.c" "CMakeFiles/libjudger.so.dir/src/rules/node.c.o" "gcc" "CMakeFiles/libjudger.so.dir/src/rules/node.c.o.d"
+  "/Users/wang/oj/Judger/src/runner.c" "CMakeFiles/libjudger.so.dir/src/runner.c.o" "gcc" "CMakeFiles/libjudger.so.dir/src/runner.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/libjudger.so.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libjudger.so.dir/src/argtable3.c.o"
+  "CMakeFiles/libjudger.so.dir/src/argtable3.c.o.d"
+  "CMakeFiles/libjudger.so.dir/src/child.c.o"
+  "CMakeFiles/libjudger.so.dir/src/child.c.o.d"
+  "CMakeFiles/libjudger.so.dir/src/killer.c.o"
+  "CMakeFiles/libjudger.so.dir/src/killer.c.o.d"
+  "CMakeFiles/libjudger.so.dir/src/logger.c.o"
+  "CMakeFiles/libjudger.so.dir/src/logger.c.o.d"
+  "CMakeFiles/libjudger.so.dir/src/main.c.o"
+  "CMakeFiles/libjudger.so.dir/src/main.c.o.d"
+  "CMakeFiles/libjudger.so.dir/src/rules/c_cpp.c.o"
+  "CMakeFiles/libjudger.so.dir/src/rules/c_cpp.c.o.d"
+  "CMakeFiles/libjudger.so.dir/src/rules/c_cpp_file_io.c.o"
+  "CMakeFiles/libjudger.so.dir/src/rules/c_cpp_file_io.c.o.d"
+  "CMakeFiles/libjudger.so.dir/src/rules/general.c.o"
+  "CMakeFiles/libjudger.so.dir/src/rules/general.c.o.d"
+  "CMakeFiles/libjudger.so.dir/src/rules/golang.c.o"
+  "CMakeFiles/libjudger.so.dir/src/rules/golang.c.o.d"
+  "CMakeFiles/libjudger.so.dir/src/rules/node.c.o"
+  "CMakeFiles/libjudger.so.dir/src/rules/node.c.o.d"
+  "CMakeFiles/libjudger.so.dir/src/runner.c.o"
+  "CMakeFiles/libjudger.so.dir/src/runner.c.o.d"
+  "libjudger.so"
+  "libjudger.so.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libjudger.so.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
